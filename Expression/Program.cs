03db5c76e9a6f6042060b5372f99120977aa84df
YuriLukashenko <yuri.lukashenko@computools.com>
2024-05-08 19:43:43 +0300

    Add project files.

---
+++ Expression/Program.cs
@@ -0,0 +1,12 @@
+﻿using Expressions;
+
+var expressions = new ExpressionService();
+
+var msDevs = expressions.GetAllPersons("Microsoft Developer").ToList();
+var appleTesters = expressions.GetAllPersons("Tester Apple ").ToList();
+
+Console.WriteLine($"Microsoft developers count: {msDevs.Count}");
+msDevs.ForEach(x => x.PrintProperties());
+
+Console.WriteLine($"Apple testers count: {appleTesters.Count}");
+appleTesters.ForEach(x => x.PrintProperties());
